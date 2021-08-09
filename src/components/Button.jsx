205db4518fc1e61cea23b8cd7234008e2ceef195
Paul <choxmail.pm@gmail.com>
2021-08-08 22:35:25 -0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import LanguageContext from '../contexts/LanguageContext'
 
-const Button = () => {
-	return (
-		<button className="ui button primary">Submit</button>
-	)
+class Button extends Component {
+    static contextType = LanguageContext
+	render() {
+		console.log(this.context)
+        return <button className="ui button primary">Submit</button>
+    }
 }
 
 export default Button
+
+
+/* static creates contextType as an instance of this(Button) class
+its the same as saying Button.contextType = LanguageContext
+
+
+*/
