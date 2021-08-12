7614ee7b9247dd2bfa47fd9122bfbf21d1aad6f2
Paul <choxmail.pm@gmail.com>
2021-08-11 18:12:20 -0700

    Update LanguageContext.js

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -2,20 +2,27 @@ import React, { useState } from 'react'
 
 const Context = React.createContext('English')
 
+export const LanguageStore = ({children}) => {
+    const [language, setLanguage] = useState('English')
 
-const LanguageStore = () => {
-	const [language, setLanguage] = useState('English')
-
-	const onLanguageChange = (language) => {
+    const onLanguageChange = (language) => {
         setLanguage(language)
         //console.log('current language', language)
     }
+
+    return (
+        <Context.Provider value={{ ...language, onLanguageChange }}>
+            {children}
+        </Context.Provider>
+    )
 }
-
-
 
 /* 
 we are creating a LanguageStore as a single source of truth. It will store the current
 language in state, and method for changing that language
 
-Context must be capitalized because */
+...language   we are taking everything in state and placing in a new spread object
+
+Context must be capitalized because anytime we create a component for react to 
+render it must be capitalized
+*/
