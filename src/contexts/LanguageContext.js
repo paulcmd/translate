d1620fd540bca0893e34bd5abfe2021cc51f4f07
Paul <choxmail.pm@gmail.com>
2021-08-12 01:14:54 -0700

    Update LanguageContext.js

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 
-const LanguageContext = React.createContext('English')
+const LanguageContext = React.createContext()
 
 export const useLanguage = () => {
 	return useContext(LanguageContext)
