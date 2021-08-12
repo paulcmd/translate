3aab98a751e2b90af75a0c1107e718d284d308a9
Paul <choxmail.pm@gmail.com>
2021-08-11 17:47:33 -0700

    setting up LanguageStore

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default React.createContext('English')
+const Context = React.createContext('English')
+
+
+const LanguageStore = () => {
+	const [language, setLanguage] = useState('English')
+
+	const onLanguageChange = (language) => {
+        setLanguage(language)
+        //console.log('current language', language)
+    }
+}
+
+
+
+/* 
+we are creating a LanguageStore as a single source of truth. It will store the current
+language in state, and method for changing that language
+
+Context must be capitalized because */
